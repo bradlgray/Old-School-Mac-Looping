e1f28b0cfe9d9e2fa472549e44f60a3feb4ca91e
Bradford Gray <bgray@westmont.edu>
2015-09-28 21:52:32 -0700

    loops through array

---
+++ Mac_Looping/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Mac_Looping
+//
+//  Created by Brad Gray on 9/28/15.
+//  Copyright © 2015 Brad Gray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
